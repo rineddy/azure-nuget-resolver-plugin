619f11040c56f2c52cc03868f4c38c5791132e57
Eddy R <rineddy@gmail.com>
2018-12-14 17:34:13 +0100

    test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Test Azure Build Pipeline Task
+
+<https://github.com/Microsoft/azure-pipelines-task-lib/blob/master/tasks.schema.json>
+
+<https://github.com/Microsoft/azure-pipelines-tasks/tree/master/Tasks/AzureFileCopyV1>
+
--- buildAndReleaseTaskPS/main.ps1
+++ buildAndReleaseTaskPS/main.ps1
@@ -1,9 +1,19 @@
+
+Import-PowerShellDataFile .\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1
+Import-Module .\ps_modules\VstsTaskSdk\VstsTaskSdk.psm1
+
 write-host "****** VARIABLES *********"
-$binDir=$env:AGENT_BINARIESDIRECTORY
-$srcDir=$env:AGENT_SOURCESDIRECTORY
+$binDir = $env:AGENT_BINARIESDIRECTORY
+$srcDir = $env:AGENT_SOURCESDIRECTORY
 write-host "srcDir = $srcDir"
 write-host "binDir = $binDir"
-$searchProjectFile=$env:INPUT_SEARCHPROJECTFILE
-$projectToTarget=$env:INPUT_PROJECTTOTARGET
+$searchProjectFile = $env:INPUT_SEARCHPROJECTFILE
+$projectToTarget = $env:INPUT_PROJECTTOTARGET
 write-host "searchProjectFile = $searchProjectFile"
 write-host "projectToTarget = $projectToTarget"
+
+write-host $(Get-TaskVariable "Agent.BinariesDirectory")
+write-host $(Get-TaskVariable "AGENT_BINARIESDIRECTORY")
+
+write-host $(Get-VstsInput "Agent.BinariesDirectory")
+write-host $(Get-VstsInput "AGENT_BINARIESDIRECTORY")
