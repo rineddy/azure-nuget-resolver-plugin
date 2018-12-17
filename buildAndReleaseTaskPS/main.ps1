6c40f0aa963ec3c415ac2486f4a40358768576d2
RinEddy <rineddy@gmail.com>
2018-12-17 03:40:54 +0100

    search file

--- buildAndReleaseTaskPS/main.ps1
+++ buildAndReleaseTaskPS/main.ps1
@@ -1,19 +1,54 @@
 
-Import-PowerShellDataFile .\ps_modules\VstsTaskSdk\VstsTaskSdk.psd1
-Import-Module .\ps_modules\VstsTaskSdk\VstsTaskSdk.psm1
+[CmdletBinding()]
+param()
 
-write-host "****** VARIABLES *********"
-$binDir = $env:AGENT_BINARIESDIRECTORY
-$srcDir = $env:AGENT_SOURCESDIRECTORY
-write-host "srcDir = $srcDir"
-write-host "binDir = $binDir"
-$searchProjectFile = $env:INPUT_SEARCHPROJECTFILE
-$projectToTarget = $env:INPUT_PROJECTTOTARGET
-write-host "searchProjectFile = $searchProjectFile"
-write-host "projectToTarget = $projectToTarget"
+# function SetProjectVariable {
+#     param(
+#         [string]$varName,
+#         [string]$varValue
+#     )
 
-write-host $(Get-TaskVariable "Agent.BinariesDirectory")
-write-host $(Get-TaskVariable "AGENT_BINARIESDIRECTORY")
+#     Write-Host ("Setting variable " + $varName + " to '" + $varValue + "'")
+#     Write-Output ("##vso[task.setvariable variable=" + $varName + ";]" + $varValue )
+# }
 
-write-host $(Get-VstsInput "Agent.BinariesDirectory")
-write-host $(Get-VstsInput "AGENT_BINARIESDIRECTORY")
+# For more information on the VSTS Task SDK:
+# https://github.com/Microsoft/vsts-task-lib
+Trace-VstsEnteringInvocation $MyInvocation
+try {
+    write-host "****** ENV *********"
+    $binDir = $env:BUILD_BINARIESDIRECTORY
+    $srcDir = $env:BUILD_SOURCESDIRECTORY
+    write-host "srcDir = $srcDir"
+    write-host "binDir = $binDir"
+
+    write-host "****** VARIABLES *********"
+    $searchProjectFile = Get-VstsInput -Name searchProjectFile -Require
+    $versionToTarget = Get-VstsInput -Name versionToTarget -Require
+    write-host "searchProjectFile = $searchProjectFile"
+    write-host "projectToTarget = $versionToTarget"
+
+    write-host "****** SEARCH PROJECT *********"
+    $filesFound = Get-ChildItem -Path $searchProjectFile -Recurse
+    if ($filesFound.Count -eq 0) { Write-Warning "No files matching pattern found." }
+    if ($filesFound.Count -gt 1) { Write-Warning "Multiple proj files found."       }
+
+    foreach ($fileName in $filesFound) {
+        Write-Host "Reading file: $($fileName.fullname)"
+        $xmlDoc = New-Object -TypeName System.Xml.XmlDocument
+        $xmlDoc.Load($fileName)
+
+        $element = [System.Xml.XmlElement]($xmlDoc.GetElementsByTagName("PackageReference") | Select-Object -First 1)
+        if ($element) {
+            write-host "Package: $($element.Attributes["Include"]) - Version: $($element.Attributes["Version"])"
+        }
+        else {
+            Write-Warning "No 'PackageReference' found in $($fileName.fullname)"
+        }
+    }
+
+}
+finally {
+    Trace-VstsLeavingInvocation $MyInvocation
+}
+
